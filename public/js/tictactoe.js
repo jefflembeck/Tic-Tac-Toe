5b1fe605083491774cb48c76b77a7d30c2fdbc8d
Jeffrey Lembeck <jlembeck@gmail.com>
2011-02-22 23:39:07 -0800

    realizing that I was overusing delegate

--- public/js/tictactoe.js
+++ public/js/tictactoe.js
@@ -1,37 +1,7 @@
 $(document).ready(function(){
   var xturn = true;
   var oturn = true;
-  $("div.space").delegate("div.space-top", "click", function(){
-    if ($(this).text() === "X" || $(this).text() === "O") {
-    } else {
-      if (xturn) {
-        $(this).text("X");
-        xturn = false;
-        oturn = true;
-      } else {
-        $(this).text("O");
-        oturn = false;
-        xturn = true;
-      }
-    }
-    checkWin();
-  })
-  $("div.space").delegate("div.space-mid", "click", function(){
-    if ($(this).text() === "X" || $(this).text() === "O") {
-    } else {
-      if (xturn) {
-        $(this).text("X");
-        xturn = false;
-        oturn = true;
-      } else {
-        $(this).text("O");
-        oturn = false;
-        xturn = true;
-      }
-    }
-    checkWin();
-  })
-  $("div.space").delegate("div.space-bot", "click", function(){
+  $("div.space").delegate("div", "click", function(){
     if ($(this).text() === "X" || $(this).text() === "O") {
     } else {
       if (xturn) {
