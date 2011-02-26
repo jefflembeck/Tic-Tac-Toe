553b3351858b30384971f71052d1fe4817582e72
Jeffrey Lembeck <jlembeck@gmail.com>
2011-02-25 19:22:08 -0800

    reload board when complete

--- public/js/tictactoe.js
+++ public/js/tictactoe.js
@@ -23,42 +23,37 @@ $(document).ready(function(){
     return true;
   }
 
+  function clearBoard() {
+    for(var i = 1; i < 10; i++) {
+      $("#" + i).text("");
+    }
+  }
+
   // If text lines up
   function checkWin() {
-    if ($('#1').text() === "X" && $('#2').text() === "X" && $('#3').text() === "X") {
-      alert("WE HAVE A WINNER!");
-    } else if ($('#4').text() === "X" && $('#5').text() === "X" && $('#6').text() === "X") {
-      alert("WE HAVE A WINNER!");
-    } else if ($('#7').text() === "X" && $('#8').text() === "X" && $('#9').text() === "X") {
-      alert("WE HAVE A WINNER!");
-    } else if ($('#1').text() === "X" && $('#4').text() === "X" && $('#7').text() === "X") {
-      alert("WE HAVE A WINNER!");
-    } else if ($('#2').text() === "X" && $('#5').text() === "X" && $('#8').text() === "X") {
-      alert("WE HAVE A WINNER!");
-    } else if ($('#3').text() === "X" && $('#6').text() === "X" && $('#9').text() === "X") {
-      alert("WE HAVE A WINNER!");
-    } else if ($('#1').text() === "X" && $('#5').text() === "X" && $('#9').text() === "X") {
-      alert("WE HAVE A WINNER!");
-    } else if ($('#3').text() === "X" && $('#5').text() === "X" && $('#7').text() === "X") {
-      alert("WE HAVE A WINNER!");
-    } else if ($('#1').text() === "O" && $('#2').text() === "O" && $('#3').text() === "O") {
-      alert("WE HAVE A WINNER!");
-    } else if ($('#4').text() === "O" && $('#5').text() === "O" && $('#6').text() === "O") {
-      alert("WE HAVE A WINNER!");
-    } else if ($('#7').text() === "O" && $('#8').text() === "O" && $('#9').text() === "O") {
-      alert("WE HAVE A WINNER!");
-    } else if ($('#1').text() === "O" && $('#4').text() === "O" && $('#7').text() === "O") {
-      alert("WE HAVE A WINNER!");
-    } else if ($('#2').text() === "O" && $('#5').text() === "O" && $('#8').text() === "O") {
-      alert("WE HAVE A WINNER!");
-    } else if ($('#3').text() === "O" && $('#6').text() === "O" && $('#9').text() === "O") {
-      alert("WE HAVE A WINNER!");
-    } else if ($('#1').text() === "O" && $('#5').text() === "O" && $('#9').text() === "O") {
-      alert("WE HAVE A WINNER!");
-    } else if ($('#3').text() === "O" && $('#5').text() === "O" && $('#7').text() === "O") {
-      alert("WE HAVE A WINNER!");
+    if (($('#1').text() === "X" && $('#2').text() === "X" && $('#3').text() === "X") ||
+        ($('#4').text() === "X" && $('#5').text() === "X" && $('#6').text() === "X") ||
+        ($('#7').text() === "X" && $('#8').text() === "X" && $('#9').text() === "X") ||
+        ($('#1').text() === "X" && $('#4').text() === "X" && $('#7').text() === "X") ||
+        ($('#2').text() === "X" && $('#5').text() === "X" && $('#8').text() === "X") ||
+        ($('#3').text() === "X" && $('#6').text() === "X" && $('#9').text() === "X") ||
+        ($('#1').text() === "X" && $('#5').text() === "X" && $('#9').text() === "X") ||
+        ($('#3').text() === "X" && $('#5').text() === "X" && $('#7').text() === "X")) {
+          alert("X wins!");
+          clearBoard();
+    } else if (($('#1').text() === "O" && $('#2').text() === "O" && $('#3').text() === "O") ||
+        ($('#4').text() === "O" && $('#5').text() === "O" && $('#6').text() === "O") ||
+        ($('#7').text() === "O" && $('#8').text() === "O" && $('#9').text() === "O") ||
+        ($('#1').text() === "O" && $('#4').text() === "O" && $('#7').text() === "O") ||
+        ($('#2').text() === "O" && $('#5').text() === "O" && $('#8').text() === "O") ||
+        ($('#3').text() === "O" && $('#6').text() === "O" && $('#9').text() === "O") ||
+        ($('#1').text() === "O" && $('#5').text() === "O" && $('#9').text() === "O") ||
+        ($('#3').text() === "O" && $('#5').text() === "O" && $('#7').text() === "O")) {
+          alert("O wins!");
+          clearBoard();
     } else if (catsGame()) {
       alert("Cat's Game. Bummer. ");
+      clearBoard();
     }
   }
 })
